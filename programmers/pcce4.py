8dc34924582c8280061238be5646ae0a8382706e
leegeon <leegun0602@naver.com>
2026-05-13 13:28:59 +0900

    [feat/#7] pcce4 기출 문제 빈칸 채우기 전

---
+++ programmers/pcce4.py
@@ -0,0 +1,13 @@
+code = input()
+last_four_words = code[-4:]
+
+if last_four_words == :
+    print("Ophthalmologyc")
+elif :
+    print("Neurosurgery")
+elif :
+    print("Orthopedics")
+:
+    print("Dermatology")
+:
+    print("direct recommendation")
