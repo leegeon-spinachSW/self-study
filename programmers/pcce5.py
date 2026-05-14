66a3fa40d4037f8dcf4fc72a1eb43b75929e4ace
leegeon <leegun0602@naver.com>
2026-05-14 14:10:29 +0900

    [feat/#9] pcce5 1차 문제 풀이

--- programmers/pcce5.py
+++ programmers/pcce5.py
@@ -1,8 +1,8 @@
 def solution(cpr):
     answer = []
     basic_order = ["check", "call", "pressure", "respiration", "repeat"]
-    for action in :
-        for i in :
+    for action in cpr:
+        for i in basic_order:
             if action == basic_order[i]:
-                answer.append()
+                answer.append(i + 1)
     return answer
