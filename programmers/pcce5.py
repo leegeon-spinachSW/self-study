e974bf6e56400f4f2da3e1d6b63cadb7c612fe81
leegeon <leegun0602@naver.com>
2026-05-14 13:43:39 +0900

    [feat/#9] pcce5 기출 문제 빈칸 채우기 전

---
+++ programmers/pcce5.py
@@ -0,0 +1,8 @@
+def solution(cpr):
+    answer = []
+    basic_order = ["check", "call", "pressure", "respiration", "repeat"]
+    for action in :
+        for i in :
+            if action == basic_order[i]:
+                answer.append()
+    return answer
